42eee37ca84684ee26a283eae9f8e1039e8df01c
Ivan Sergeev <ivan.sergeev@ist.ac.at>
2024-11-03 10:55:28 +0100

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -11,3 +11,9 @@ require "leanprover-community" / "mathlib" @ git "master"
 lean_lib MatroidDecompositionTheoremVerification {
   globs := #[.submodules `MatroidDecompositionTheoremVerification]
 }
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
