67e4e6cad887a47b1addc9c4af4d333cdb34e34c
madvorak <martin.dvorak@matfyz.cz>
2024-11-16 13:46:15 +0100

    cleanup in MatrixTU

--- Seymour/ForMathlib/MatrixTU.lean
+++ Seymour/ForMathlib/MatrixTU.lean
@@ -45,12 +45,8 @@ lemma Matrix.TU_iff (A : Matrix X Y R) : A.TU ↔
 
 lemma Matrix.TU.apply {A : Matrix X Y R} (hA : A.TU) (i : X) (j : Y) :
     A i j = 0 ∨ A i j = 1 ∨ A i j = -1 := by
-  let f : Fin 1 → X := (fun _ => i)
-  let g : Fin 1 → Y := (fun _ => j)
-  convert hA 1 f g (Function.injective_of_subsingleton f) (Function.injective_of_subsingleton g) <;>
-  exact (det_fin_one (A.submatrix f g)).symm
-
-#check abs_eq_abs
+  rw [Matrix.TU_iff] at hA
+  convert hA 1 (fun _ => i) (fun _ => j) <;> simp
 
 lemma Matrix.TU.submatrix {A : Matrix X Y R} (hA : A.TU) (k : ℕ) (f : Fin k → X) (g : Fin k → Y) :
     (A.submatrix f g).TU := by
@@ -102,8 +98,6 @@ lemma Matrix.mapEquiv_both_TU {X' Y' : Type*} [DecidableEq X'] [DecidableEq Y']
     Matrix.TU ((A · ∘ eY) ∘ eX) ↔ A.TU := by
   rw [Matrix.mapEquiv_rows_TU, Matrix.mapEquiv_cols_TU]
 
-#check Matrix.reindex
-
 lemma Matrix.TU_adjoin_row0s_iff (A : Matrix X Y R) {X' : Type*} :
     (Matrix.fromRows A (Matrix.row X' 0)).TU ↔ A.TU := by
   rw [Matrix.TU_iff, Matrix.TU_iff]
@@ -126,52 +120,6 @@ lemma Matrix.TU_adjoin_row0s_iff (A : Matrix X Y R) {X' : Type*} :
         ext
         apply hf₀
       apply hA
-
-example {m n : ℕ} (A : Matrix (Fin m) (Fin n) R) (j' : Fin n) :
-    (Matrix.of (Matrix.vecCons (fun j : Fin n => if j = j' then 1 else 0) A)).TU ↔
-    (Matrix.of (Matrix.vecCons 0 A)).TU := by
-  rw [Matrix.TU_iff, Matrix.TU_iff]
-  constructor <;> intro hA k f g
-  · if used_row : ∃ i, f i = 0 then
-      obtain ⟨i, hi⟩ := used_row
-      left
-      apply Matrix.det_eq_zero_of_row_eq_zero i
-      intro
-      simp_all
-    else
-      obtain ⟨f₀, hf₀⟩ : ∃ f₀ : Fin k → Fin m, f = Fin.castSucc ∘ f₀
-      · use fun i =>
-          match hfi : (f i).val with
-          | 0 => False.elim (used_row ⟨i, Fin.eq_of_val_eq hfi⟩)
-          | i' + 1 => ⟨i', by omega⟩
-        ext i
-        simp
-        sorry
-      specialize hA k f g
-      have key (v : Fin n → R) :
-        ((Matrix.of (Matrix.vecCons v A)).submatrix (Fin.castSucc ∘ f₀) g) =
-        (((Matrix.of (Matrix.vecCons v A)).submatrix Fin.castSucc id).submatrix f₀ g)
-      · simp
-      have wtf (v : Fin n → R) :
-        ((Matrix.of (Matrix.vecCons v A)).submatrix Fin.castSucc id) = A
-      · ext i j
-        simp [Matrix.vecCons]
-        apply congr_fun
-        --rw [Fin.cons_succ]
-        sorry
-      rwa [hf₀, key, wtf] at hA ⊢
-  · if used_row : ∃ i, f i = 0 then
-      obtain ⟨i, hi⟩ := used_row
-      if hits_one : ∃ j, g j = j' then
-        -- TODO Laplacian expansion
-        sorry
-      else -- copypaste
-        left
-        apply Matrix.det_eq_zero_of_row_eq_zero i
-        intro
-        simp_all
-    else
-      sorry
 
 lemma Matrix.TU_adjoin_rowUnit_aux (A : Matrix X Y R) (j' : Y) [DecidableEq Y] :
     (Matrix.fromRows A (Matrix.row Unit (fun j : Y => if j = j' then 1 else 0))).TU ↔
@@ -247,22 +195,22 @@ lemma Matrix.TU_adjoin_id_right_iff [DecidableEq X] [DecidableEq Y] (A : Matrix 
   convert Matrix.TU_adjoin_id_left_iff A
   aesop
 
+omit X Y
 variable {X₁ X₂ Y₁ Y₂ : Type*}
 
-lemma Matrix.submatrix_fromBlocks {α ι γ : Type*} (f : ι → X₁ ⊕ X₂) (g : γ → Y₁ ⊕ Y₂)
-    (A₁₁ : Matrix X₁ Y₁ α) (A₁₂ : Matrix X₁ Y₂ α) (A₂₁ : Matrix X₂ Y₁ α) (A₂₂ : Matrix X₂ Y₂ α) :
-    (Matrix.fromBlocks A₁₁ A₁₂ A₂₁ A₂₂).submatrix f g =
-    (fun (i : ι) (j : γ) =>
-      match f i with
-      | .inl i₁ =>
-        match g j with
-        | .inl j₁ => A₁₁ i₁ j₁
-        | .inr j₂ => A₁₂ i₁ j₂
-      | .inr i₂ =>
-        match g j with
-        | .inl j₁ => A₂₁ i₂ j₁
-        | .inr j₂ => A₂₂ i₂ j₂
-    ) := by
+lemma Matrix.fromBlocks_submatrix_apply {α ι γ : Type*} (f : ι → X₁ ⊕ X₂) (g : γ → Y₁ ⊕ Y₂)
+    (A₁₁ : Matrix X₁ Y₁ α) (A₁₂ : Matrix X₁ Y₂ α) (A₂₁ : Matrix X₂ Y₁ α) (A₂₂ : Matrix X₂ Y₂ α) (i : ι) (j : γ) :
+    (Matrix.fromBlocks A₁₁ A₁₂ A₂₁ A₂₂).submatrix f g i j =
+    match f i with
+    | .inl i₁ =>
+      match g j with
+      | .inl j₁ => A₁₁ i₁ j₁
+      | .inr j₂ => A₁₂ i₁ j₂
+    | .inr i₂ =>
+      match g j with
+      | .inl j₁ => A₂₁ i₂ j₁
+      | .inr j₂ => A₂₂ i₂ j₂
+    := by
   aesop
 
 lemma Matrix.fromBlocks_TU {A₁ : Matrix X₁ Y₁ R} {A₂ : Matrix X₂ Y₂ R} (hA₁ : A₁.TU) (hA₂ : A₂.TU) :
